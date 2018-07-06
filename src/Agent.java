e99cdd87b7fcb55f182581661f03047316189ea1
Junyu Lin(Frank) <396685004@qq.com>
2018-07-06 20:02:54 +0800

    Update Agent.java

--- src/Agent.java
+++ src/Agent.java
@@ -1,22 +1,3 @@
-
-/**
- * Hao Chen z5077894 group with junyu Lin z5077890
- * In our design, we build our global map. Everything in the view that we had would be added into our global map. Every vertex has its attitudes 
- * including the "searchValue" which means the value of visiting this vertex based on our special value algorithm. Our algorithm will calculate the 
- * search value according to the distance between vertex and agent, the distance between the vertex and existed key, axe, door, tree and stone. So 
- * every existed key, axe, door, tree and stone will influence the search value of other vertexes in global map.
- * 
- * At the same time, the agent has some state: "its position (x,y) in our global map", "the flags about whether it has key, axes, stone or raft".
- * 
- * Our agent will mark visited vertex and keep to visit the unvisited vertex in our global map (except for something not possible to be visited like wall).
- * We will search unvisited vertex in our global map as the order from high search value to low search value (which is calculated by our special algorithm)
- * After finding the vertex we want, we will use astar algorithm to get the path and do the corresponding action to visit it.
- * 
- * If agent has axe, it can chop the tree and if the agent has the key, it can open the door. 
- * If the tree or door is chopped or open, it will become a floor in our global map.
- * If the agent has raft, it can get into sea if necessary.
- * If the agent has the stone, it will put the stone in the necessary place. After the stone is placed, it will become the "floor" in our global map.
- */
 import java.util.*;
 import java.io.*;
 import java.net.*;
